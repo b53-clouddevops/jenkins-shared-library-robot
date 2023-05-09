79e7f241d941e026d73e3884ac976a26a68b83cf
CodingManoj <mvp2612@hotmail.com>
2023-05-08 22:35:03 -0400

    Git Auto Push

--- vars/common.groovy
+++ vars/common.groovy
@@ -115,7 +115,7 @@ if(env.UPLOAD_STATUS == "") {
       stage('Uploading the artifact') {
           withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'NEXUS_PSW', usernameVariable: 'NEXUS_USR')]) {
 
-            sh "curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://${NEXUS_URL}:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"                
+            sh "curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.13.88:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"                
           
           } 
         }

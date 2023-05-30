66d21884dd1ac8b71642613985bd885a01e7683b
CodingManoj <mvp2612@hotmail.com>
2023-05-29 23:18:10 -0400

    Git Auto Push

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -36,13 +36,13 @@ def call() {
                     sh "docker tag 834725375088.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest 834725375088.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}"
                     sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 834725375088.dkr.ecr.us-east-1.amazonaws.com"
                     sh "docker push 834725375088.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:{TAG_NAME}"
+                }
             }
-       }
+        }
+
         // env.APP_TYPE = "maven" 
         // common.lintChecks()
         // env.ARGS="-Dsonar.java.binaries=target/"
         // common.sonarChecks()
         // common.testCases()
         // common.artifacts()
-    }
-}

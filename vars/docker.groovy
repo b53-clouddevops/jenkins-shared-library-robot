c8548f14203e68ce0765e397085c668cd237ac80
CodingManoj <mvp2612@hotmail.com>
2023-05-29 23:18:53 -0400

    Git Auto Push

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -32,7 +32,7 @@ def call() {
 
                     '''
                 }
-                    sh "docker build -t 834725375088.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest"
+                    sh "docker build -t 834725375088.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest ."
                     sh "docker tag 834725375088.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest 834725375088.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}"
                     sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 834725375088.dkr.ecr.us-east-1.amazonaws.com"
                     sh "docker push 834725375088.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:{TAG_NAME}"
